e4573492c23e8b710dccf1a8bc6e46c5142082d0
Jansen Ducusin <senducusin@gmail.com>
2021-02-04 12:08:53 +0800

    Added logic for querying each order by ID or sequence

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const app = express()
 
+var orders = [
+    {
+        "name":"Hawaiian Delight",
+        "size":"Large"
+    },{
+        "name":"Classic Cheese",
+        "size":"Party"
+    }
+]
+
 app.get('/', (req,rest) => {
-    rest.send('Hello World')
+    rest.send('Hello World!')
 })
 
 app.get('/api/orders', (req,res) => {
-    res.send([1,2,3])
+    res.send(orders)
 })
 
+app.get('/api/orders/:id', (req,res) => {
+    const orderId = req.params.id - 1
 
-app.listen(3000, ()=> console.log('Listening on port 3000...'))
+    console.log(`${orderId} - ${orders.length - 1}: ${orderId <= (orders.length - 1)}`)
+
+    if (orderId > (orders.length - 1) || (orderId < 0)) {
+        res.status(404).send("That order ID is not found")
+    }else{
+        res.send(orders[orderId])
+    }
+})
+
+// Uses set port else custom (3000)
+const port = process.env.PORT || 3000
+app.listen(port, ()=> console.log(`Listening on port ${port}...`))
