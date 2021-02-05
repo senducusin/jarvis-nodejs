6541ce073cbf308c694d905f166352b6e4888771
Jansen Ducusin <senducusin@gmail.com>
2021-02-05 10:01:22 +0800

    Added req.ip on logs

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ app.use(express.json())
 var orders = []
 
 app.get('/', (req,res) => {
+    console.log(`Request from ${req.ip}`)
     res.redirect("https://senducusin.github.io/portfolio/")
 })
 
 app.get('/api/pizza/orders', (req,res) => {
+    console.log(`Request from ${req.ip}`)
     res.send(orders)
 })
 
 app.get('/api/pizza/orders/:id', (req,res) => {
     const orderId = req.params.id - 1
 
-    console.log(`${orderId} - ${orders.length - 1}: ${orderId <= (orders.length - 1)}`)
+    console.log(`Request from ${req.ip}`)
 
     if (orderId > (orders.length - 1) || (orderId < 0)) {
         res.status(404).send("That order ID is not found")
@@ -27,6 +29,8 @@ app.get('/api/pizza/orders/:id', (req,res) => {
 
 app.post('/api/pizza/order',(req,res) => {
     const body = req.body
+
+    console.log(`Request from ${req.ip}`)
 
     if(
         body.hasOwnProperty("name") === true && 
