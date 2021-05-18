ce4181b482667a64b485a6b64e0a8a650da4748f
Jansen Ducusin <senducusin@gmail.com>
2021-05-18 10:09:41 +0800

    Create new account

--- routes/bank.js
+++ routes/bank.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const app = express()
 const router = express.Router()
 const { v4: uuidv4 } = require('uuid');
+
+app.use(express.json())
 
 var accounts = [
     {
@@ -18,6 +21,24 @@ var accounts = [
 ]
 
 router.get('/api/bank/accounts', (req, res) => {
+    res.json(accounts)
+})
+
+router.post('/api/bank/create', (req, res) => {
+
+    console.log(req.body)
+    let name = req.body.name
+    let accountType = req.body.accountType
+    let balance = req.body.balance
+
+    let account = {
+        name: name,
+        accountType: accountType,
+        balance: balance
+    }
+
+    accounts.push(account)
+
     res.json(accounts)
 })
 
