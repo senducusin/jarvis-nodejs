ce383f5b0cfc71b911129c6c1a960b5d23b90e8d
Jansen Ducusin <senducusin@gmail.com>
2021-06-01 11:25:49 +0800

    Added websocket services

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+const WebSocket = require("ws")
+
+const server = require("http").createServer(app)
+const wss = new WebSocket.Server({ server: server })
 
 const stocksRoutes = require('./routes/stocks');
 const topNewsRoutes = require('./routes/topNews');
 const pizzaRoutes = require('./routes/pizza');
 const bankRoutes = require('./routes/bank').router;
+
+wss.on("connection", function connection(ws) {
+
+    console.log("A new client conencted")
+
+    ws.send("Welcome New Client!")
+
+    ws.on("message", function incoming(message) {
+        console.log("received: %s", message)
+        ws.send("Got your message: " + message)
+    })
+})
 
 app.use(express.json())
 
@@ -20,4 +36,4 @@ app.use(bankRoutes);
 
 // Uses set port else custom (3000)
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+server.listen(port, () => console.log(`Listening on port ${port}...`))
